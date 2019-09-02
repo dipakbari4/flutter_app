fe163b4ac92723213cc564006b6682d137ffced2
Dipak Bari <dipakbari4@gmail.com>
2019-09-02 20:37:26 +0530

    1st step with flutter. changes something with custom class and assigned it into runApp method.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text("Let's start fluttering!", ),
-));
+void main() => runApp(MyApp()); // MyApp class
+
+
+//The below class extends the StatelessWidget
+class MyApp extends StatelessWidget {
+
+  // Creating the single individual instance of MyApp class
+  @override
+  Widget build(BuildContext context) {
+    // This will return the Material App to MyApp class
+    return MaterialApp(
+      home: Text("Let's start fluttering!",),
+    );
+  }
+
+}
