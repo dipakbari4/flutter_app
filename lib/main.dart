ed202119feefd09629aff9d520139914f8fe8d4d
Dipak Bari <dipakbari4@gmail.com>
2019-09-02 21:22:14 +0530

    English words library added and applied on AppBar and center widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
-import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart'; // importing new library for experimenting
 
 void main() => runApp(MyApp()); // MyApp class
 
-
 //The below class extends the StatelessWidget
 class MyApp extends StatelessWidget {
-
+  final wordPair = WordPair.random(); // Getting the random strings
+  final message = WordPair.random(); // Getting the random strings
   // Creating the single individual instance of MyApp class
   @override
   Widget build(BuildContext context) {
     // This will return the Material App to MyApp class
     return MaterialApp(
-      home: Text("Let's start fluttering!",),
+        home: new Scaffold(
+          appBar: AppBar(
+            title: Text(wordPair.asCamelCase),
+          ),
+          body: Center(
+            child: Text(message.asPascalCase),
+          ),
+        )
     );
   }
-
 }
